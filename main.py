ae45864ae49ed5d37e127593e5937ffff3c1baa2
Mandevinchik <dvilper@mail.ru>
2025-03-22 11:42:02 +0500

    Add auto restart steam after swtich account reg

--- main.py
+++ main.py
@@ -1,34 +1,51 @@
 import winreg, keyboard, subprocess, json
-
+from time import sleep
 
 def get_config():
     with open("config.json", "r") as f:
         return json.load(f)
     
-def set_active_account(account):
+def setKey(key, value):
     config = get_config()
-    config["active_account"] = account
+    config[key] = value
+    with open("config.json", "w") as f:
+        json.dump(config, f, indent=4)
+    
+def getKey(key: str):
+    config = get_config()
+    return config[key]
+
+def add_account(new_account):
+    config = get_config()
+    config["accounts"].append(str(new_account))
     with open("config.json", "w") as f:
         json.dump(config, f, indent=4)
 
 def get_steam_path():
     try:
-        key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, r"SOFTWARE\Valve\Steam")
-        steam_path = winreg.QueryValueEx(key, "InstallPath")[0]
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"SOFTWARE\Valve\Steam", 0, winreg.KEY_ALL_ACCESS)
+        steam_path = winreg.QueryValueEx(key, "SteamExe")[0]
         winreg.CloseKey(key)
         return steam_path
+    
     except Exception as e:
         print(f"Error getting Steam path: {e}")
         return None
 
-def switch_steam_account(username):
-    active_account = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Valve\Steam", 0, winreg.KEY_ALL_ACCESS)
-    set_active_account(username)
-    winreg.SetValueEx(active_account, "AutoLoginUser", 0, winreg.REG_SZ, username)
-    winreg.CloseKey(active_account)
+def switch_steam_account(username: str):
+    try:
+        autoLoginUserRef = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Valve\Steam", 0, winreg.KEY_ALL_ACCESS)
+        active_account = winreg.QueryValueEx(autoLoginUserRef, "AutoLoginUser")[0]
+        print("Change account from: ", active_account)
 
-# def main(): 
-#     keyboard.add_hotkey("ctrl+shift+l")
-# switch_steam_account(get_steam_path())
+        winreg.SetValueEx(autoLoginUserRef, "AutoLoginUser", 0, winreg.REG_SZ, username)
+        winreg.CloseKey(autoLoginUserRef)
+        setKey("active_account",username)
 
-set_active_account("mandanin1")
+        subprocess.Popen(["start", get_steam_path(), "-shutdown"], shell=True)
+        sleep(2)
+        subprocess.Popen(["start", "steam://open"], shell=True)
+        
+    except Exception as e:
+        print(f"Error switching steam account: {e}")
+        

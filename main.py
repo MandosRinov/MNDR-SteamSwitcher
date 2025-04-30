8dd475c82dd5b098bd3f8bf1c4d020f7c5067d77
MNDR <dvilper@mail.ru>
2025-05-01 01:15:00 +0300

    remake account loging function and refactor code

---
+++ main.py
@@ -0,0 +1,126 @@
+from steam_switcher import *
+import json, subprocess, winreg, sys, os, re
+
+
+def get_config():
+    try: 
+        with open("./config.json", "r") as f:
+            return json.load(f)
+    except:
+        with open("./config.json", "w") as f:
+            config = {
+                "active_account": "",
+                "accounts": []
+            }
+            json.dump(config, f, indent=4)
+        return get_config()
+    
+def get_accounts():
+    loginUsersPath = get_steam_path() + "\\config\\loginusers.vdf"
+    if (os.path.exists(loginUsersPath)):
+        with open(loginUsersPath, "r+", encoding="utf8") as file:
+            file_text = file.read()
+            accountsName_raw = re.findall(r'"AccountName"\t\t"[a-z0-1]{1,}"', file_text)
+            allowAutoLogin_raw = re.finditer(r'"AllowAutoLogin"\t\t"[0-1]"', file_text)
+
+            for autoLogin in allowAutoLogin_raw:
+                file.seek(autoLogin.span()[1]-2)
+                print(file.read(1))
+        accountsName = [accountName_raw.split("\t\t")[1] for accountName_raw in accountsName_raw]
+
+        
+
+        print(accountsName)
+        
+
+
+    
+def setKey(key, value):
+    config = get_config()
+    config[key] = value
+    with open("./config.json", "w") as f:
+        json.dump(config, f, indent=4)
+    
+def getKey(key: str):
+    config = get_config()
+    return config[key]
+
+
+def switch_steam_account(username: str):
+    try:
+        autoLoginUserRef = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Valve\Steam", 0, winreg.KEY_ALL_ACCESS)
+        active_account = winreg.QueryValueEx(autoLoginUserRef, "AutoLoginUser")[0]
+        if active_account == username:
+            print("You already on this account")
+            exit()
+        print("Change account from: ", active_account , " to ", username)
+
+        winreg.SetValueEx(autoLoginUserRef, "AutoLoginUser", 0, winreg.REG_SZ, username)
+        winreg.CloseKey(autoLoginUserRef)
+
+        if steam_running():
+            print("Steam is running") 
+            subprocess.run(["start", get_steamexe_path(), "-shutdown"], shell=True, creationflags=0x08000000, check=True)
+            sleep(2)
+            counter = 0
+            while steam_running():
+                if counter <= 10:
+                    counter += 1
+                    sleep(1)
+                    continue
+                else:
+                    print("Steam still running!")
+        
+
+        
+        subprocess.run("start steam://open/main", shell=True, check=True)
+        print("Launching steam ...")
+        sleep(4)
+        
+        
+        
+    except Exception as e:
+        print("Error switching steam account:", e)
+
+
+def main(): 
+    accounts_list = getKey("accounts")
+    get_accounts()
+    if len(sys.argv) == 1:
+        pass
+    else: 
+        if sys.argv[1] in accounts_list:
+            switch_steam_account(sys.argv[1])
+        else:
+            print("Invalid argument(s): ", *sys.argv)
+    
+
+if __name__ == "__main__":
+    main()
+
+
+# "users"
+# {
+# 	"76561199405860839"
+# 	{
+# 		"AccountName"		"mandanin1"
+# 		"PersonaName"		"MandaRinchikS"
+# 		"RememberPassword"		"1"
+# 		"WantsOfflineMode"		"0"
+# 		"SkipOfflineModeWarning"		"0"
+# 		"AllowAutoLogin"		"0"
+# 		"MostRecent"		"0"
+# 		"Timestamp"		"1746046184"
+# 	}
+# 	"76561198415075961"
+# 	{
+# 		"AccountName"		"kapralgames"
+# 		"PersonaName"		"MaND3ve"
+# 		"RememberPassword"		"1"
+# 		"WantsOfflineMode"		"0"
+# 		"SkipOfflineModeWarning"		"0"
+# 		"AllowAutoLogin"		"0"
+# 		"MostRecent"		"1"
+# 		"Timestamp"		"1746048733"
+# 	}
+# }
--- steam_switcher.py
+++ steam_switcher.py
@@ -1,48 +1,5 @@
-import winreg, subprocess, json, psutil as ps, sys
+import winreg, psutil as ps
 from time import sleep
-
-
-def get_config():
-    try: 
-        with open("./config.json", "r") as f:
-            return json.load(f)
-    except:
-        with open("./config.json", "w") as f:
-            config = {
-                "active_account": "",
-                "accounts": []
-            }
-            json.dump(config, f, indent=4)
-        return get_config()
-    
-def setKey(key, value):
-    config = get_config()
-    config[key] = value
-    with open("./config.json", "w") as f:
-        json.dump(config, f, indent=4)
-    
-def getKey(key: str):
-    config = get_config()
-    return config[key]
-
-def add_account(new_account):
-    config = get_config()
-    config["accounts"].append(str(new_account))
-    with open("./config.json", "w") as f:
-        json.dump(config, f, indent=4)
-    print("Account added")
-
-def del_account(account : list):
-    config = get_config()
-    accounts  = config["accounts"]
-    print(f"Deleting account: {account} ...")
-    for i in range(len(accounts)):
-        if accounts[i] == account:
-            accounts.pop(i)
-            break
-    with open('./config.json', "w") as f:
-        json.dump(config, f, indent=4)
-    print("Account deleted!")
 
 def steam_running():
     key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"SOFTWARE\Valve\Steam\ActiveProcess", 0, winreg.KEY_ALL_ACCESS)
@@ -63,7 +20,7 @@ def steam_running():
     except ps.NoSuchProcess:
         return False
 
-def get_steam_path():
+def get_steamexe_path():
     try:
         key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"SOFTWARE\Valve\Steam", 0, winreg.KEY_ALL_ACCESS)
         raw_path = winreg.QueryValueEx(key, "SteamExe")[0]
@@ -81,64 +38,25 @@ def get_steam_path():
     except Exception as e:
         print(f"Error getting Steam path: {e}")
         return None
-
-def switch_steam_account(username: str):
-    try:
-        autoLoginUserRef = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Valve\Steam", 0, winreg.KEY_ALL_ACCESS)
-        active_account = winreg.QueryValueEx(autoLoginUserRef, "AutoLoginUser")[0]
-        setKey("active_account", active_account)
-        if active_account == username:
-            print("You alright on this account")
-            exit()
-        print("Change account from: ", active_account , " to ", username)
-
-        winreg.SetValueEx(autoLoginUserRef, "AutoLoginUser", 0, winreg.REG_SZ, username)
-        winreg.CloseKey(autoLoginUserRef)
-        setKey("active_account",username)
-
-        if steam_running():
-            print("Steam is running") 
-            subprocess.run(["start", get_steam_path(), "-shutdown"], shell=True, creationflags=0x08000000, check=True)
-            sleep(2)
-            counter = 0
-            while steam_running():
-                if counter <= 10:
-                    counter += 1
-                    sleep(1)
-                    continue
-                else:
-                    print("Steam still running!")
-        
-
-        
-        subprocess.run("start steam://open/main",
-                   shell=True, check=True)
-        print("Launching steam ...")
-        sleep(4)
-        
-        
-    except Exception as e:
-        print(f"Error switching steam account: {e}")
-        
-
-
-def main(): 
-    accounts_list = getKey("accounts")
-    if len(sys.argv) == 1:
-        pass
-    else: 
-        if sys.argv[1] in accounts_list:
-            switch_steam_account(sys.argv[1])
-        elif sys.argv[1] in ("-a", '--add') and sys.argv[2]:
-            add_account(sys.argv[2].strip())
-        elif sys.argv[1] in ("-d", '--delete') and sys.argv[2]:
-            del_account(sys.argv[2].strip())
-        elif sys.argv[1] in ("-w", '--write'):
-            print("Accounts: " + str(*accounts_list))
-        else:
-            print("Invalid argument: ", sys.argv)
-            print("Possible you dont have any accounts")
     
+def get_steam_path():
+    try:
+        key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"SOFTWARE\Valve\Steam", 0, winreg.KEY_ALL_ACCESS)
+        raw_path = winreg.QueryValueEx(key, "SteamPath")[0]
+        winreg.CloseKey(key)
+        raw_path_items = raw_path.split('/')
+        path_items = []
+        for item in raw_path_items:
+            if ' ' in item:
+                path_items.append(f'"{item}"')
+            else:
+                path_items.append(item)
+        steam_exe = "\\".join(path_items)
+        return steam_exe
+    
+    except Exception as e:
+        print(f"Error getting Steam path: {e}")
+        return None
 
-if __name__ == "__main__":
-    main()
+
+        
